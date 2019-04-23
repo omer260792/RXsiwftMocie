4c452b8abc46353a56b6042fa44037e0fff9e57f
Omer Cohen <omer@msapps.mobi>
2019-04-23 14:48:55 +0300

    Initial Commit

---
+++ MovieListRXSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieListRXSwift
+//
+//  Created by Omer Cohen on 4/23/19.
+//  Copyright © 2019 Omer Cohen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
